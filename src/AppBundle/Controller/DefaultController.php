469be03da351b7a8aa1cb06bbd83586a36c139e2
Nodir Rashidov <nrashidov@yahoo.com>
2018-04-29 14:39:58 +0900

    login, register, tos screens done

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,14 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+    /**
+     * @Route("/tos", name="tos")
+     */
+    public function tosAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render(':default/Tos:tos.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
 }
--- src/AppBundle/Form/RegistrationType.php
+++ src/AppBundle/Form/RegistrationType.php
@@ -10,17 +10,77 @@ namespace AppBundle\Form;
 
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name');
-        $builder->add('phone');
-        $builder->add('address');
+        $builder
+            ->add('username', TextType::class, array(
+                'label' => 'Username',
+                'attr' => array(
+                    'class' => 'input',
+                    'placeholder' => "Username"
+                )
+            ))
+            ->add('email', EmailType::class, array(
+                'label' => 'Email',
+                'attr' => array(
+                    'class' => 'input',
+                    'placeholder' => "Agency email address"
+                )
+            ))
+            ->add('plainPassword', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'options' => array(
+                    'attr' => array(
+                        'autocomplete' => 'new-password',
+                    ),
+                ),
+                'first_options' => array(
+                    'label' => 'Password',
+                    'attr' => array(
+                        'class' => 'input',
+                        'placeholder' => "Password"
+                    )
+                ),
+                'second_options' => array(
+                    'label' => 'Repeat password',
+                    'attr' => array(
+                        'class' => 'input',
+                        'placeholder' => "Repeat password"
+                    )
+                ),
+                'invalid_message' => 'fos_user.password.mismatch',
+            ))
+            ->add('name', TextType::class, array(
+                'label' => 'Agency name',
+                'attr' => array(
+                    'class' => 'input',
+                    'placeholder' => "Agency name"
+                )
+            ))
+            ->add('phone', TextType::class, array(
+                'label' => 'Phone number',
+                'attr' => array(
+                    'class' => 'input',
+                    'placeholder' => "Agency phone number"
+                )
+            ))
+            ->add('address', TextType::class, array(
+                'label' => 'Address',
+                'attr' => array(
+                    'class' => 'input',
+                    'placeholder' => "Agency address"
+                )
+            ));
     }
+
     public function getParent()
     {
         return 'FOS\UserBundle\Form\Type\RegistrationFormType';
