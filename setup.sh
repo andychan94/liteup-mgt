055d25f3f555cd13a2ce91b7395aecb01b22ac73
Nodir Rashidov <nrashidov@yahoo.com>
2018-05-11 14:13:07 +0900

    setup.sh

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 LINEA="RewriteCond %{HTTPS} off"
 LINEB="RewriteRule ^(.*)$ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]"
 
-sed -i '22i\'$LINEA'\' newfile.txt
-sed -i '22i\'$LINEB'\' newfile.txt
+sed -i "22i $LINEA" public_html/.htaccess
+sed -i "23i $LINEB" public_html/.htaccess
